ebf9b0066d2f6322940b2742eaa3dd43246a1a6b
Myrotvorets <support@myrotvorets.center>
2022-10-23 15:34:20 +0300

    Redo instrumentation

--- src/lib/tracing.ts
+++ src/lib/tracing.ts
@@ -2,8 +2,8 @@
 
 import { EventEmitter } from 'events';
 import { OpenTelemetryConfigurator } from '@myrotvorets/opentelemetry-configurator';
-import { KnexInstrumentation } from '@myrotvorets/opentelemetry-plugin-knex';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { KnexInstrumentation } from '@opentelemetry/instrumentation-knex';
 
 if (+(process.env.ENABLE_TRACING || 0)) {
     EventEmitter.defaultMaxListeners += 5;
@@ -13,7 +13,7 @@ export async function configure(): Promise<void> {
     if (+(process.env.ENABLE_TRACING || 0)) {
         const configurator = new OpenTelemetryConfigurator({
             serviceName: 'bot/myrotvorets.news',
-            instrumentations: [...getNodeAutoInstrumentations(), new KnexInstrumentation()],
+            instrumentations: [new HttpInstrumentation(), new KnexInstrumentation()],
         });
 
         await configurator.start();
